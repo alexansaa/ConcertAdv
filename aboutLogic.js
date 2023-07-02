54e220ac05f9b0c9621604989279e6017718e6ee
alexansaa <alexan.saa@gmail.com>
2023-07-01 20:44:34 -0500

    Styling fix and home link add

--- aboutLogic.js
+++ aboutLogic.js
@@ -6,5 +6,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const about = document.querySelector('#about');
     about.addEventListener('click', () => {
         window.location.href = "about.html";
+    });
+    const home = document.querySelector('#home');
+    home.addEventListener('click', () => {
+        window.location.href = "index.html";
     })
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const linksElements = document.querySelectorAll('#myLinks a');
+    const openBtn = document.querySelector('#menuHamburgerContainer');
+    const myLinksContainer = document.querySelector('#myLinks');
+    const myIcon = document.querySelector('#menuHamburgerContainer');
+    const myIconClose = document.querySelector('#closeMenuImage');
+    openBtn.addEventListener('click', () => {
+        myLinksContainer.classList.add('openable');
+        myIcon.classList.add('disaperable');
+        myIconClose.classList.add('apearable');
+    });
+    const closeBtn = document.querySelector('#closeHamburger');
+  closeBtn.addEventListener('click', () => {
+    myLinksContainer.classList.remove('openable');
+    myIcon.classList.remove('disaperable');
+  });
+  linksElements.forEach((e) => {
+    e.addEventListener('click', () => {
+      myLinksContainer.classList.remove('openable');
+      myIcon.classList.remove('disaperable');
+    });
+  });
 });
